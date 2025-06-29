c65ae05a03bb6488c16f5ef1a842a2e4ddb83c7e
Namitha_Shaik <naminamitha921@gmail.com>
2025-06-29 12:32:02 +0530

    Create user.model.js

---
+++ Project Files/backend/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const schema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+schema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+schema.methods.matchPassword = function (raw) {
+  return bcrypt.compare(raw, this.password);
+};
+
+export default mongoose.model("User", schema);
